fa05995e4adaeaedc1efdcd662fb30dd1d7f54f3
TechnoStudy <info@techno.study>
2020-03-05 21:23:55 -0500

    init

--- src/test/java/MongoTest.java
+++ src/test/java/MongoTest.java
@@ -3,13 +3,20 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class MongoTest {
+    private MongoDatabase database;
+
+    @BeforeClass
+    public void init() {
+        MongoClient mongoClient = MongoClients.create("mongodb://techno:ee4CvCRPhor5@185.97.114.201:27118/?authSource=cloud-school");
+        database = mongoClient.getDatabase("cloud-school");
+    }
+
     @Test
     public void test() {
-        MongoClient mongoClient = MongoClients.create("mongodb://techno:ee4CvCRPhor5@185.97.114.201:27118/?authSource=cloud-school");
-        MongoDatabase database = mongoClient.getDatabase("cloud-school");
         MongoCollection<Document> collection = database.getCollection("school_city");
         System.out.println(collection.countDocuments());
     }
