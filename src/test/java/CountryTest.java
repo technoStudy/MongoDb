808b469f0f4f6b106746267e6c7e9ecff5fdb3c4
TechnoStudy <info@techno.study>
2020-03-07 14:21:36 -0500

    copy country test

---
+++ src/test/java/CountryTest.java
@@ -0,0 +1,200 @@
+import io.restassured.http.ContentType;
+import model.Country;
+import org.testng.annotations.Test;
+import utilities.BaseTest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CountryTest extends BaseTest {
+    @Test
+    public void getBasePath() {
+        given()
+                .when()
+                .log().body()
+                .get()
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    @Test
+    public void getCountries() {
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .get( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+
+    @Test
+    public void createCountry() {
+        Country country = new Country();
+        country.setName( name );
+        country.setCode( code );
+
+        // creating country
+        String countryId = given()
+                .cookies( cookies )
+                .body( country )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // deleting country
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( "/school-service/api/countries/" + countryId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    @Test
+    public void editTest() {
+        Country country = new Country();
+        country.setName( name );
+        country.setCode( code );
+
+        // creating country
+        String countryId = given()
+                .cookies( cookies )
+                .body( country )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // Editing country
+        country.setId( countryId );
+        country.setName( nameEdited );
+        country.setCode( codeEdited );
+        given()
+                .cookies( cookies )
+                .body( country )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .put( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+                .body( "name", equalTo( country.getName() ) )
+                .body( "code", equalTo( country.getCode() ) )
+        ;
+
+        // deleting country
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( "/school-service/api/countries/" + countryId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    @Test
+    public void createCountryNegativeTest() {
+        Country country = new Country();
+        country.setName( name );
+        country.setCode( code );
+
+        // creating country
+        String countryId = given()
+                .cookies( cookies )
+                .body( country )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        given()
+                .cookies( cookies )
+                .body( country )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 400 );
+
+        // deleting country
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( "/school-service/api/countries/" + countryId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+    }
+
+    @Test
+    public void deleteCountryNegativeTest() {
+        Country country = new Country();
+        country.setName( name );
+        country.setCode( code );
+
+        // creating country
+        String countryId = given()
+                .cookies( cookies )
+                .body( country )
+                .contentType( ContentType.JSON )
+                .when()
+                .log().body()
+                .post( "/school-service/api/countries" )
+                .then()
+                .log().body()
+                .statusCode( 201 )
+                .extract().jsonPath().getString( "id" );
+
+        // deleting country
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( "/school-service/api/countries/" + countryId )
+                .then()
+                .log().body()
+                .statusCode( 200 )
+        ;
+
+        // deleting country again
+        given()
+                .cookies( cookies )
+                .when()
+                .log().body()
+                .delete( "/school-service/api/countries/" + countryId )
+                .then()
+                .log().body()
+                .statusCode( 404 )
+        ;
+    }
+
+}
